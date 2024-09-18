82f5381dd4d36b592520f3cb085277d214ef65e4
hadi rahjoo <rahjooh@yahoo.com>
2024-09-18 19:28:16 +0330

    Add files via upload
    sales-api/main added
    logger.go added
    ..

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/rahjooh/service/foundation/logger"
+	"go.uber.org/zap"
+)
+
+func main(){
+	// Construct the application logger.
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+func run(log *zap.SugaredLogger) error {
+	
+	// =========================================================================
+	// GOMAXPROCS
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// =========================================================================
+	// GOMAXPROCS
+	
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdown
+
+	log.Infow("shutdown","status","shutdown started","signal",sig)
+	defer log.Infow("shutdown","status","shutdown compelete","signal",sig)
+
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rahjooh/service
+
+go 1.22.5
+
+require (
+	github.com/ardanlabs/conf v1.5.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
